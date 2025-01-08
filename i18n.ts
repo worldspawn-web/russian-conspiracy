300d354560c646f786d2eec9e47881f8cc7c773a
Michael Lozitckii <worldspawn.web@gmail.com>
2025-01-09 00:37:38 +0300

    fix: 🐛 fixed 'NO_I18NEXT_INSTANCE' error

--- i18n.ts
+++ i18n.ts
@@ -1,19 +1,24 @@
+'use client';
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import translationEN from '@/locales/en.json';
 import translationRU from '@/locales/ru.json';
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: translationEN,
-    },
-    ru: {
-      translation: translationRU,
-    },
+// the translations
+const resources = {
+  en: {
+    translation: translationEN,
   },
-  lng: 'ru',
+  ru: {
+    translation: translationRU,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'ru', // default language
   fallbackLng: 'ru',
   interpolation: {
     escapeValue: false,
